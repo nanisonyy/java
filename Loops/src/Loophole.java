3ab4fbd9ba5fcb12150c3181e803486b8a771fc5
harsha <harsha@DESKTOP-T4BEBEI>
2016-01-30 20:06:56 -0500

    Loops

---
+++ Loops/src/Loophole.java
@@ -0,0 +1,19 @@
+
+public class Loophole {
+public static void main(String[] arg)
+{
+	for(int i=1;i<=4;i++)
+	{
+		for(int j=1;j<=4;j++)
+		{
+			int k=i+j-1;
+			if(k>4){
+			System.out.print(k-4);
+			}else{
+			System.out.print(k);
+			}
+		}
+		System.out.println();
+	}
+}
+}
---
+++ Loops/src/Pattern.java
@@ -0,0 +1,16 @@
+
+public class Pattern {
+	public static void main(String[] arg)
+	{
+		for(int i=0;i<4;i++)
+		{
+			for(int j=0;j<=i;j++)
+			{
+				System.out.print(j);
+			}
+			System.out.println(" ");
+		
+		}
+	}
+
+}
